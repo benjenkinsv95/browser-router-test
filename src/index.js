4034fb72735bc2068133d776a02a04bc8851eb96
benjenkinsv95 <benjenkinsv95@gmail.com>
2020-09-23 11:56:38 -0400

    Add newer approach to hosting on gh-pages

--- src/index.js
+++ src/index.js
@@ -5,9 +5,8 @@ import './index.scss'
 import App from './components/App/App'
 import { BrowserRouter } from 'react-router-dom'
 
-console.log('PUBLIC_URL', process.env.PUBLIC_URL)
 const appJsx = (
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 )
